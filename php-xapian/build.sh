9d67e859fe60b80c33383b2cab6637928c699796
Sam Pearson <sam@sgp.me.uk>
2019-04-30 16:28:03 +0100

    [builds] Abort if docker builds fails for php7-xapian

--- php-xapian/build.sh
+++ php-xapian/build.sh
@@ -3,6 +3,11 @@
 echo "==> Building docker image..."
 docker build -t php-xapian-builder:latest .
 
+if [ $? -ne 0 ]; then
+  echo "==> Build Failed, aborting."
+  exit 1
+fi
+
 echo "==> Building software..."
 docker run -v ${PWD}/deb:/home/builder/deb php-xapian-builder:latest
 
