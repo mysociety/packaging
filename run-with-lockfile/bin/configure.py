c1cca7df8bb1824b304374b54e35aca62326985d
Sam Pearson <sam@sgp.me.uk>
2021-02-04 16:32:41 +0000

    [builds] Update configure script for jshint and run-with-lockfile
    Uses the global variables - also adds some of these to the Dockerfile
    templates.

--- run-with-lockfile/bin/configure.py
+++ run-with-lockfile/bin/configure.py
@@ -5,9 +5,27 @@ import sys
 from jinja2 import Environment, FileSystemLoader
 import yaml
 
+# Props: https://stackoverflow.com/questions/823196/yaml-merge-in-python
+def merge(user, default):
+    if isinstance(user,dict) and isinstance(default,dict):
+        for k,v in default.items():
+            if k not in user:
+                user[k] = v
+            else:
+                user[k] = merge(user[k],v)
+    return user
+
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 targets = yaml.safe_load(open(os.path.join(parent_dir, '../distributions.yaml')))
-conf_data = yaml.safe_load(open(os.path.join(parent_dir, 'conf/conf.yaml')))
+global_data = yaml.safe_load(open(os.path.join(parent_dir, '../globals.yaml')))
+
+if os.path.isfile(os.path.join(parent_dir, 'conf/conf.yaml')):
+    local_data = yaml.safe_load(open(os.path.join(parent_dir, 'conf/conf.yaml')))
+else:
+    local_data = {}
+
+conf_data = merge(local_data, global_data)
+
 env = Environment(loader = FileSystemLoader(os.path.join(parent_dir, 'conf/')), trim_blocks=True, lstrip_blocks=True)
 template = env.get_template('Dockerfile.j2')
 
